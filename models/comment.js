4888a2c06acb4cac3493a21fc445412130253893
ramirezjag00 <33412680+ramirezjag00@users.noreply.github.com>
2017-11-20 17:30:45 +0800

    Add files via upload

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,16 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
 	text:String,
-	author:String
+	author:{
+		id:{
+			type:mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username:Number,
+		name: String,
+		lname:String
+	},
+	created:{type:Date, default: Date.now}
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
