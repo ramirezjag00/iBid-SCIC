b6fb00f91b6158c1f751ccfc4e42a362e44e5e3f
ramirezjag00 <33412680+ramirezjag00@users.noreply.github.com>
2017-11-21 12:51:42 +0800

    Add files via upload

---
+++ middleware/index.js
@@ -0,0 +1,53 @@
+//all the middleware goes here
+var Item = require("../models/item");
+var Comment = require("../models/comment");
+var middlewareObj = {};
+
+middlewareObj.checkItemOwnership = function(req,res,next) {
+		if(req.isAuthenticated()){
+			Item.findById(req.params.id, function(err, foundItem){
+				if(err){
+				res.redirect("back");
+				} else {
+					//does user own the item?
+				if(foundItem.author.id.equals(req.user._id)){
+					next();
+				} else {
+					res.redirect("back");
+				}
+				}
+			});
+	} else {
+		res.redirect("back");
+	}
+} 
+
+
+
+middlewareObj.checkCommentOwnership = function(req,res,next) {
+		if(req.isAuthenticated()){
+			Comment.findById(req.params.comment_id, function(err, foundComment){
+				if(err){
+				res.redirect("back");
+				} else {
+					//does user own the comment?
+				if(foundComment.author.id.equals(req.user._id)){
+					next();
+				} else {
+					res.redirect("back");
+				}
+				}
+			});
+	} else {
+		res.redirect("back");
+	}
+} 
+
+middlewareObj.isLoggedIn = function(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+module.exports = middlewareObj;
--- models/item.js
+++ models/item.js
@@ -3,8 +3,28 @@ var mongoose = require("mongoose");
 
 var itemSchema = new mongoose.Schema({
 	name:String,
+	price: Number,
+	bidPrice: Number,
+	highestBidder: {
+		id:{
+			type:mongoose.Schema.Types.ObjectId,
+			ref:"User"
+		},
+		username:Number,
+		name:String
+	},
 	image:String,
 	description: String,
+	created:{type:Date, default: Date.now},
+	endDate:{type:Date, default: +new Date() + 7*24*60*60*1000},
+	author: {
+		id:{
+			type:mongoose.Schema.Types.ObjectId,
+			ref:"User"
+		},
+		username:Number,
+		name:String
+	},
 	comments:[
 	{
 		type: mongoose.Schema.Types.ObjectId,
